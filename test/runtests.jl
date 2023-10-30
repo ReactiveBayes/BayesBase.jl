b6fd0c7547df5144c0fae2a7879ffae256071ca6
Bagaev Dmitry <bvdmitri@gmail.com>
2023-10-30 16:18:40 +0100

    fix runtests script on M2

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Aqua, CpuId, ReTestItems, BayesBase
 # `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
 Aqua.test_all(BayesBase; ambiguities=false, piracy=false)
 
+nthreads = max(cputhreads(), 1)
+ncores = max(cpucores(), 1)
+
 runtests(
-    BayesBase;
-    nworkers=cpucores(),
-    nworker_threads=Int(cputhreads() / cpucores()),
-    memory_threshold=1.0,
+    BayesBase; nworkers=ncores, nworker_threads=Int(nthreads / ncores), memory_threshold=1.0
 )
