d5c19d922a451043d66a4e0d5b89b2a1b74cbfd3
polym (Tim) <t.n.nisslbeck@tue.nl>
2025-10-22 09:54:14 +0200

    41 Use Hwloc instead of CpuId in test/runtests.jl ... (#42)
    * #41 Use of Hwloc instead of CpuId for cross-platform/architecture-agnostic detection of cores/threads.
    
    * Run make format

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Aqua, CpuId, ReTestItems, BayesBase
+using Aqua, Hwloc, ReTestItems, BayesBase
 
 # `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
 # `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
@@ -9,9 +9,8 @@ Aqua.test_all(
     deps_compat=(; check_extras=false, check_weakdeps=true),
 )
 
-nthreads = max(cputhreads(), 1)
-ncores = max(cpucores(), 1)
+ncores = max(Hwloc.num_physical_cores(), 1)
+nthreads = max(Hwloc.num_virtual_cores(), 1)
+threads_per_core = max(Int(floor(nthreads / ncores)), 1)
 
-runtests(
-    BayesBase; nworkers=ncores, nworker_threads=Int(nthreads / ncores), memory_threshold=1.0
-)
+runtests(BayesBase; nworkers=ncores, nworker_threads=threads_per_core, memory_threshold=1.0)
