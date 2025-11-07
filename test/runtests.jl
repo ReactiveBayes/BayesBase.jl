38799ec1fc59f3274a62ecd34f6801555ede5592
Tim <t.n.nisslbeck@tue.nl>
2025-11-07 07:11:00 +0100

    #43 Add make test test_args=... filter.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,52 @@
 using Aqua, Hwloc, ReTestItems, BayesBase
 
-# `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
-# `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
-Aqua.test_all(
-    BayesBase;
-    ambiguities=false,
-    piracies=false,
-    deps_compat=(; check_extras=false, check_weakdeps=true),
-)
+if get(ENV, "RUN_AQUA", "true") == "true"
+    # `ambiguities = false` - there are quite some ambiguities, but these should be normal and should not be encountered under normal circumstances
+    # `piracy = false` - we extend/add some of the methods to the objects defined in the Distributions.jl
+    Aqua.test_all(
+        BayesBase;
+        ambiguities=false,
+        piracies=false,
+        deps_compat=(; check_extras=false, check_weakdeps=true),
+    )
+end
 
-ncores = max(Hwloc.num_physical_cores(), 1)
-nthreads = max(Hwloc.num_virtual_cores(), 1)
-threads_per_core = max(Int(floor(nthreads / ncores)), 1)
+nthreads, ncores = Hwloc.num_virtual_cores(), Hwloc.num_physical_cores()
+nthreads, ncores = max(nthreads, 1), max(ncores, 1)
+nworker_threads = Int(nthreads / ncores)
+memory_threshold = 1.0
 
-runtests(BayesBase; nworkers=ncores, nworker_threads=threads_per_core, memory_threshold=1.0)
+pkg_root = dirname(dirname(pathof(BayesBase)))
+test_root = joinpath(pkg_root, "test")
+
+if isempty(ARGS)
+    runtests(
+        BayesBase;
+        nworkers=ncores,
+        nworker_threads=nworker_threads,
+        memory_threshold=memory_threshold,
+    )
+else
+    for arg in ARGS
+        # Translate colon syntax (e.g., rules:normal_mean_variance → rules/normal_mean_variance)
+        candidate = join(split(arg, ":"), "/")
+
+        # Build possible test paths relative to the package test directory
+        paths = [joinpath(test_root, candidate), joinpath(test_root, candidate * ".jl")]
+
+        path = findfirst(ispath, paths)
+
+        if path !== nothing
+            selected_path = paths[path]
+            @info "Running selective tests from $selected_path"
+            runtests(
+                selected_path;
+                nworkers=ncores,
+                nworker_threads=nworker_threads,
+                memory_threshold=memory_threshold,
+            )
+        else
+            @warn "Test target not found: $arg"
+        end
+    end
+end
